dcaddd6b0c3c80713cc34333cb5df55d2920dae6
piotrek <pjastrzebscy@gmail.com>
2019-12-01 14:14:04 +0100

    login

--- src/main/java/pl/jaszczomb/appserverside/controller/UserController.java
+++ src/main/java/pl/jaszczomb/appserverside/controller/UserController.java
@@ -22,21 +22,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
-        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if(errorMap != null) return errorMap;
 
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsername(),
-                        loginRequest.getPassword()
-                )
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = TOKEN_PREFIX +  tokenProvider.generateToken(authentication);
-
-        return ResponseEntity.ok(new JWTLoginSucessReponse(true, jwt));
     }
 
     @GetMapping
